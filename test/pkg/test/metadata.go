00420a4b6e9e11ee8707423560244cb07c127a50
Ellis Tarn <ellistarn@gmail.com>
2023-03-15 10:32:02 -0700

    test: Implemented e2e test framework (#118)
    * test: Implemented e2e test framework
    
    * test: upgraded tests to v1beta1

---
+++ test/pkg/test/metadata.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+
+	randomdata "github.com/Pallinder/go-randomdata"
+	"github.com/imdario/mergo"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	sequentialNumber     = 0
+	randomizer           = rand.New(rand.NewSource(time.Now().UnixNano())) //nolint
+	sequentialNumberLock = new(sync.Mutex)
+	DiscoveryLabel       = "testing.kubernetes.io"
+)
+
+func New[T client.Object](t T, mergeFrom ...T) T {
+	t.SetName(RandomName())
+	t.SetNamespace("default")
+	t.SetLabels(map[string]string{DiscoveryLabel: "true"})
+	return MustMerge(t, mergeFrom...)
+}
+
+func MustMerge[T interface{}](dest T, srcs ...T) T {
+	for _, src := range srcs {
+		if err := mergo.Merge(&dest, src, mergo.WithOverride, mergo.WithAppendSlice); err != nil {
+			panic(fmt.Sprintf("Failed to merge object: %s", err))
+		}
+	}
+	return dest
+}
+
+func RandomName() string {
+	sequentialNumberLock.Lock()
+	defer sequentialNumberLock.Unlock()
+	sequentialNumber++
+	return strings.ToLower(fmt.Sprintf("%s-%d-%s", randomdata.SillyName(), sequentialNumber, randomdata.Alphanumeric(10)))
+}
