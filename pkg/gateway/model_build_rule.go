4c8bfd4621f8b6d3e674afbd70d53903625e2052
liwen wu <liwenwu@amazon.com>
2023-03-17 20:07:58 +0000

    Add parsing header-match rule from HTTPRoute object

--- pkg/gateway/model_build_rule.go
+++ pkg/gateway/model_build_rule.go
@@ -2,14 +2,31 @@ package gateway
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/golang/glog"
 
+	"github.com/aws/aws-sdk-go/aws"
+
 	latticemodel "github.com/aws/aws-application-networking-k8s/pkg/model/lattice"
+	"github.com/aws/aws-sdk-go/service/vpclattice"
+	gateway_api "sigs.k8s.io/gateway-api/apis/v1beta1"
 )
 
 const (
 	resourceIDRuleConfig = "RuleConfig"
+	// error code
+	LATTICE_NO_SUPPORT_FOR_MULTIPLE_MATCHES = "LATTICE_NO_SUPPORT_FOR_MULTIPLE_MATCHES"
+
+	LATTICE_EXCEED_MAX_HEADER_MATCHES = "LATTICE_EXCEED_MAX_HEADER_MATCHES"
+
+	LATTICE_UNSUPPORTED_MATCH_TYPE = "LATTICE_UNSUPPORTED_MATCH_TYPE"
+
+	LATTICE_UNSUPPORTED_HEADER_MATCH_TYPE = "LATTICE_UNSUPPORTED_HEADER_MATCH_TYPE"
+
+	LATTICE_UNSUPPORTED_PATH_MATCH_TYPE = "LATTICE_UNSUPPORTED_PATH_MATCH_TYPE"
+
+	LATTICE_MAX_HEADER_MATCHES = 5
 )
 
 func (t *latticeServiceModelBuildTask) buildRules(ctx context.Context) error {
@@ -26,14 +43,67 @@ func (t *latticeServiceModelBuildTask) buildRules(ctx context.Context) error {
 		}
 
 		for _, httpRule := range t.httpRoute.Spec.Rules {
-			glog.V(6).Infof("buildRoutingPolicy, examping rules spec: %v\n", httpRule)
+			glog.V(2).Infof("liwwu>>buildRoutingPolicy, examing rules spec: %v\n", httpRule)
 			var ruleValue string
+			var ruleSpec latticemodel.RuleSpec
 
-			if len(httpRule.Matches) != 0 {
-				// only handle 1 match for now using  path
-				if httpRule.Matches[0].Path.Value != nil {
-					ruleValue = *httpRule.Matches[0].Path.Value
+			if len(httpRule.Matches) == 0 || len(httpRule.Matches) > 1 {
+				// only support 1 match today
+				return errors.New(LATTICE_NO_SUPPORT_FOR_MULTIPLE_MATCHES)
+			}
+
+			// only support 1 match today
+			match := httpRule.Matches[0]
+
+			if match.Path != nil && match.Path.Type != nil {
+				glog.V(2).Infof("liwwu>> Using pathmatch %v for for httproute %s namespace %s ",
+					*match.Path, t.httpRoute.Name, t.httpRoute.Namespace)
+
+				switch *match.Path.Type {
+				case gateway_api.PathMatchExact:
+					ruleSpec.PathMatchExact = true
+
+				case gateway_api.PathMatchPathPrefix:
+					ruleSpec.PathMatchPrefix = true
+				default:
+					glog.V(2).Infof("Unsupported path match type %v for httproute %s namespace %s",
+						*match.Path.Type, t.httpRoute.Name, t.httpRoute.Namespace)
+					return errors.New(LATTICE_UNSUPPORTED_PATH_MATCH_TYPE)
 				}
+			}
+
+			// header based match
+			// today, only support EXACT match
+			if match.Headers != nil {
+				if len(match.Headers) > LATTICE_MAX_HEADER_MATCHES {
+					return errors.New(LATTICE_EXCEED_MAX_HEADER_MATCHES)
+				}
+
+				ruleSpec.NumOfHeaderMatches = len(match.Headers)
+
+				glog.V(2).Infof("liwwu>> match.Headers %v", match.Headers)
+				for i, header := range match.Headers {
+					glog.V(2).Infof("liwwu>>> i = %d header.Type %v", i, *header.Type)
+					if header.Type != nil && gateway_api.HeaderMatchType(*header.Type) != gateway_api.HeaderMatchExact {
+						glog.V(2).Infof("Unsupported header matchtype %v for httproute %v namespace %s",
+							*header.Type, t.httpRoute.Name, t.httpRoute.Namespace)
+						return errors.New(LATTICE_UNSUPPORTED_HEADER_MATCH_TYPE)
+					}
+
+					glog.V(2).Infof("HeaderExactMatch==%v for HTTPRoute %v, namespace %v", header.Value, t.httpRoute.Name, t.httpRoute.Namespace)
+
+					matchType := vpclattice.HeaderMatchType{
+						Exact: aws.String(header.Value),
+					}
+					ruleSpec.MatchedHeaders[i].Match = &matchType
+				}
+			}
+
+			// controller do not support these match type today
+			if match.Method != nil || match.QueryParams != nil {
+				glog.V(2).Infof("Unsupported Match Method %v for httproute %v, namespace %v",
+					match.Method, t.httpRoute.Name, t.httpRoute.Namespace)
+				return errors.New(LATTICE_UNSUPPORTED_MATCH_TYPE)
 			}
 
 			tgList := []*latticemodel.RuleTargetGroup{}
