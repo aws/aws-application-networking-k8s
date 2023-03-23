8e38a256e854c2309fb3dedaa8bd490a05ac7dc3
liwen wu <liwenwu@amazon.com>
2023-03-23 05:44:22 +0000

    Fix unit test failures

--- pkg/model/lattice/rule.go
+++ pkg/model/lattice/rule.go
@@ -36,11 +36,6 @@ type RuleSpec struct {
 	NumOfHeaderMatches int `json:"numofheadermatches"`
 	MatchedHeaders     [MAX_NUM_OF_MATCHED_HEADERS]vpclattice.HeaderMatch
 
-	// TODO delete RuleType and RuleValue
-	/*
-		RuleType   string     `json:"ruletype"`
-		RuleValue  string     `json:"value"`
-	*/
 	RuleID     string     `json:"id"`
 	Action     RuleAction `json:"action"`
 	CreateTime time.Time  `json:"time"`
