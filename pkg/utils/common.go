e70e87f241e307cef21ad86fcafe4ea84c701dfc
liwen wu <liwenwu@amazon.com>
2023-03-30 18:06:49 +0000

    Remove reference to mercury

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -1,7 +1,5 @@
 package utils
 
-// {"arn":"arn:aws:mercury:us-east-1:649797057964:mesh/mesh-0f94edeeb1f6d63c6","id":"mesh-0f94edeeb1f6d63c6","name":"20220120mesh"}
-
 // TODO: should be check by API call (Mingxi)
 func ArntoId(arn string) string {
 	if len(arn) == 0 {
