9f5c7af89fe53bd01524800d6f4e259e8bbeecd4
Zijun Wang <zijunw@amazon.com>
2023-04-11 13:35:37 -0700

    - Improved Test framework, adding VPC Lattice resource thorough clean up logic in the AfterSuite()
    - Trace each test run created k8s and VPC Lattice resource, properly clean up them in AfterSuite()
    
    - Add more retry and wait by Eventually(), base on vpc lattice api throttling setting and async api time spent
    
    - Add HeaderMatch httproute integ-test case

---
+++ test/pkg/test/header_match_httproute.go
@@ -0,0 +1,55 @@
+package test
+
+import (
+	"github.com/aws/aws-application-networking-k8s/pkg/latticestore"
+	"github.com/samber/lo"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/gateway-api/apis/v1beta1"
+)
+
+func (env *Framework) NewHeaderMatchHttpRoute(parentRefsGateway *v1beta1.Gateway, services []*v1.Service) *v1beta1.HTTPRoute {
+	var rules []v1beta1.HTTPRouteRule
+	for _, service := range services {
+		rule := v1beta1.HTTPRouteRule{
+			BackendRefs: []v1beta1.HTTPBackendRef{{
+				BackendRef: v1beta1.BackendRef{
+					BackendObjectReference: v1beta1.BackendObjectReference{
+						Name: v1beta1.ObjectName(service.Name),
+						Kind: lo.ToPtr(v1beta1.Kind("Service")),
+					},
+				},
+			}},
+			Matches: []v1beta1.HTTPRouteMatch{
+				{
+					Headers: []v1beta1.HTTPHeaderMatch{
+						{
+							Type:  lo.ToPtr(v1beta1.HeaderMatchExact),
+							Name:  "my-header-name1",
+							Value: "my-header-value1",
+						},
+						{
+							Type:  lo.ToPtr(v1beta1.HeaderMatchExact),
+							Name:  "my-header-name2",
+							Value: "my-header-value2",
+						},
+					},
+				},
+			},
+		}
+		rules = append(rules, rule)
+	}
+	httpRoute := New(&v1beta1.HTTPRoute{
+		Spec: v1beta1.HTTPRouteSpec{
+			CommonRouteSpec: v1beta1.CommonRouteSpec{
+				ParentRefs: []v1beta1.ParentReference{{
+					Name:        v1beta1.ObjectName(parentRefsGateway.Name),
+					SectionName: lo.ToPtr(v1beta1.SectionName("http")),
+				}},
+			},
+			Rules: rules,
+		},
+	})
+
+	env.TestCasesCreatedServiceNames[latticestore.AWSServiceName(httpRoute.Name, httpRoute.Namespace)] = true
+	return httpRoute
+}
