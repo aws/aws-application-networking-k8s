b038a3c450dbf55d8db1717da97a52574c5562a7
Salman Esmaili <>
2023-06-28 06:41:02 +0000

    Added integration tests

--- test/pkg/test/service_export_import.go
+++ test/pkg/test/service_export_import.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"strconv"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/mcs-api/pkg/apis/v1alpha1"
@@ -39,4 +41,47 @@ func (env *Framework) CreateServiceExportAndServiceImportByService(service *v1.S
 	})
 	env.TestCasesCreatedK8sResource = append(env.TestCasesCreatedK8sResource, serviceExport, serviceImport)
 	return serviceExport, serviceImport
+}
+
+func (env *Framework) CreateServiceExport(service *v1.Service) *v1alpha1.ServiceExport {
+	serviceExport := New(&v1alpha1.ServiceExport{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "multicluster.x-k8s.io/v1alpha1",
+			Kind:       "ServiceExport",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      service.Name,
+			Namespace: service.Namespace,
+			Annotations: map[string]string{
+				"multicluster.x-k8s.io/federation": "amazon-vpc-lattice",
+				"multicluster.x-k8s.io/port":       strconv.FormatInt(int64(service.Spec.Ports[0].Port), 10),
+			},
+		},
+	})
+	env.TestCasesCreatedK8sResource = append(env.TestCasesCreatedK8sResource, serviceExport)
+	return serviceExport
+}
+
+func (env *Framework) CreateServiceImport(service *v1.Service) *v1alpha1.ServiceImport {
+	serviceImport := New(&v1alpha1.ServiceImport{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "multicluster.x-k8s.io/v1alpha1",
+			Kind:       "ServiceImport",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      service.Name,
+			Namespace: service.Namespace,
+		},
+		Spec: v1alpha1.ServiceImportSpec{
+			Type: v1alpha1.ClusterSetIP,
+			Ports: []v1alpha1.ServicePort{
+				{
+					Port:     service.Spec.Ports[0].Port,
+					Protocol: service.Spec.Ports[0].Protocol,
+				},
+			},
+		},
+	})
+	env.TestCasesCreatedK8sResource = append(env.TestCasesCreatedK8sResource, serviceImport)
+	return serviceImport
 }
