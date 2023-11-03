16916d8e764f5d529108e1add50b0e21e31fd1b9
Zijun Wang <32318664+zijun726911@users.noreply.github.com>
2023-11-02 17:37:18 -0700

    Change Chunks() unit test (#473)
    Co-authored-by: Zijun Wang <zijunw@amazon.com>

--- pkg/utils/common_test.go
+++ pkg/utils/common_test.go
@@ -1,78 +1,51 @@
 package utils
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 )
 
 func TestChunks(t *testing.T) {
-	obj1 := &struct{}{}
-	obj2 := &struct{}{}
-	obj3 := &struct{}{}
-	obj4 := &struct{}{}
-	obj5 := &struct{}{}
 
-	type testCase struct {
-		name string
-		in   []*struct{}
-		size int
-		want [][]*struct{}
+	type test struct {
+		sliceLen         int
+		chunkSize        int
+		wantChunksLen    int
+		wantLastChunkLen int
 	}
-	tests := []testCase{
-		{
-			name: "No items, no chunks",
-			in:   []*struct{}{},
-			size: 2,
-			want: [][]*struct{}{},
-		},
-		{
-			name: "single item, single chunk: [obj1] -> [[obj1]]",
-			in:   []*struct{}{obj1},
-			size: 2,
-			want: [][]*struct{}{{obj1}},
-		},
-		{
-			name: "[obj1, obj2] -> [[obj1,obj2]]",
-			in:   []*struct{}{obj1, obj2},
-			size: 2,
-			want: [][]*struct{}{{obj1, obj2}},
-		},
-		{
-			name: "[obj1, obj2, obj3] -> [[obj1,obj2],[obj3]]",
-			in:   []*struct{}{obj1, obj2, obj3},
-			size: 2,
-			want: [][]*struct{}{{obj1, obj2}, {obj3}},
-		},
-		{
-			name: "[obj1, obj2, obj3, obj4] -> [[obj1,obj2],[obj3,obj4]]",
-			in:   []*struct{}{obj1, obj2, obj3, obj4},
-			size: 2,
-			want: [][]*struct{}{{obj1, obj2}, {obj3, obj4}},
-		},
-		{
-			name: "[obj1, obj2, obj3, obj4, obj5] -> [[obj1,obj2],[obj3,obj4],[obj5]]",
-			in:   []*struct{}{obj1, obj2, obj3, obj4, obj5},
-			size: 2,
-			want: [][]*struct{}{{obj1, obj2}, {obj3, obj4}, {obj5}},
-		},
-		{
-			name: "size 0",
-			in:   []*struct{}{obj1, obj2, obj3, obj4, obj5},
-			size: 0,
-			want: [][]*struct{}{},
-		},
-		{
-			name: "negative size",
-			in:   []*struct{}{obj1, obj2, obj3, obj4, obj5},
-			size: -1,
-			want: [][]*struct{}{},
-		},
+
+	tests := []test{
+		{0, -1, 0, 0},
+		{0, 0, 0, 0},
+		{0, 1, 0, 0},
+		{1, 1, 1, 1},
+		{2, 1, 2, 1},
+		{10, 2, 5, 2},
+		{102, 10, 11, 2},
 	}
+
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Chunks(tt.in, tt.size); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Chunks() got %v, want %v", got, tt.want)
+		t.Run(fmt.Sprintf("sliceLen=%d, chunkSize=%d", tt.sliceLen, tt.chunkSize), func(t *testing.T) {
+			slice := makeIntSlice(tt.sliceLen)
+			chunks := Chunks(slice, tt.chunkSize)
+			gotChunksLen := len(chunks)
+			if gotChunksLen != tt.wantChunksLen {
+				t.Errorf("number of chunks does not match, want=%d, got=%d", tt.wantChunksLen, gotChunksLen)
+			}
+			if gotChunksLen > 0 {
+				lastChunk := chunks[gotChunksLen-1]
+				if len(lastChunk) != tt.wantLastChunkLen {
+					t.Errorf("last chunk size does not match, want=%d, got=%d", tt.wantLastChunkLen, len(lastChunk))
+				}
 			}
 		})
 	}
+}
+
+func makeIntSlice(s int) []int {
+	out := make([]int, s)
+	for i := 0; i < s; i++ {
+		out[i] = i
+	}
+	return out
 }
