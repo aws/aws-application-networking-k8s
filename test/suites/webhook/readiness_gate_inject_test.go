5640db211064922bce6f5e108997485b518e48ae
erikfu <erikfu@amazon.com>
2024-02-27 09:55:47 -0800

    webhook integration test stability improvements

--- test/suites/webhook/readiness_gate_inject_test.go
+++ test/suites/webhook/readiness_gate_inject_test.go
@@ -9,6 +9,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"time"
 )
 
 var _ = Describe("Readiness Gate Inject", Ordered, func() {
@@ -27,59 +28,56 @@ var _ = Describe("Readiness Gate Inject", Ordered, func() {
 	}
 
 	BeforeAll(func() {
-		err1 := testFramework.Client.Create(ctx, untaggedNS)
-		err2 := testFramework.Client.Create(ctx, taggedNS)
-		if err1 != nil || err2 != nil {
-			Fail("unable to create test namespaces")
-		}
+		testFramework.ExpectDeletedThenNotFound(ctx, untaggedNS, taggedNS)
+		testFramework.ExpectCreated(ctx, untaggedNS, taggedNS)
 	})
 
 	It("create deployment in untagged namespace, no readiness gate", func() {
 		deployment, _ := testFramework.NewHttpApp(test.HTTPAppOptions{Name: "untagged-test-pod", Namespace: untaggedNS.Name})
-		testFramework.ExpectCreated(ctx, deployment)
-		testFramework.Get(ctx, types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}, deployment)
+		Eventually(func(g Gomega) {
+			testFramework.ExpectCreated(ctx, deployment)
 
-		pods := testFramework.GetPodsByDeploymentName(deployment.Name, deployment.Namespace)
-		Expect(len(pods)).To(BeEquivalentTo(1))
+			testFramework.Get(ctx, types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}, deployment)
 
-		pod := pods[0]
-		pct := corev1.PodConditionType(webhook.PodReadinessGateConditionType)
+			pods := testFramework.GetPodsByDeploymentName(deployment.Name, deployment.Namespace)
+			g.Expect(len(pods)).To(BeEquivalentTo(1))
 
-		for _, rg := range pod.Spec.ReadinessGates {
-			if rg.ConditionType == pct {
-				Fail("Pod readiness gate was injected to unlabeled namespace")
+			pod := pods[0]
+			pct := corev1.PodConditionType(webhook.PodReadinessGateConditionType)
+
+			for _, rg := range pod.Spec.ReadinessGates {
+				if rg.ConditionType == pct {
+					g.Expect(true).To(BeFalse(), "Pod readiness gate was injected to unlabeled namespace")
+				}
 			}
-		}
+		}).WithTimeout(30 * time.Second).WithOffset(1).Should(Succeed())
 	})
 
 	It("create deployment in tagged namespace, has readiness gate", func() {
 		deployment, _ := testFramework.NewHttpApp(test.HTTPAppOptions{Name: "tagged-test-pod", Namespace: taggedNS.Name})
-		testFramework.ExpectCreated(ctx, deployment)
-		testFramework.Get(ctx, types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}, deployment)
+		Eventually(func(g Gomega) {
+			testFramework.ExpectCreated(ctx, deployment)
+			testFramework.Get(ctx, types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}, deployment)
 
-		pods := testFramework.GetPodsByDeploymentName(deployment.Name, deployment.Namespace)
-		Expect(len(pods)).To(BeEquivalentTo(1))
+			pods := testFramework.GetPodsByDeploymentName(deployment.Name, deployment.Namespace)
+			g.Expect(len(pods)).To(BeEquivalentTo(1))
 
-		pod := pods[0]
-		pct := corev1.PodConditionType(webhook.PodReadinessGateConditionType)
+			pod := pods[0]
+			pct := corev1.PodConditionType(webhook.PodReadinessGateConditionType)
 
-		foundCount := 0
-		for _, rg := range pod.Spec.ReadinessGates {
-			if rg.ConditionType == pct {
-				foundCount++
+			foundCount := 0
+			for _, rg := range pod.Spec.ReadinessGates {
+				if rg.ConditionType == pct {
+					foundCount++
+				}
 			}
-		}
 
-		if foundCount != 1 {
-			Fail(fmt.Sprintf("Pod readiness gate was expected on labeled namespace. Found %d times", foundCount))
-		}
+			g.Expect(foundCount).To(Equal(1),
+				fmt.Sprintf("Pod readiness gate was expected on labeled namespace. Found %d times", foundCount))
+		}).WithTimeout(30 * time.Second).WithOffset(1).Should(Succeed())
 	})
 
 	AfterAll(func() {
-		err1 := testFramework.Client.Delete(ctx, untaggedNS)
-		err2 := testFramework.Client.Delete(ctx, taggedNS)
-		if err1 != nil || err2 != nil {
-			testFramework.Log.Warn("unable to delete test namespaces")
-		}
+		testFramework.ExpectDeletedThenNotFound(ctx, untaggedNS, taggedNS)
 	})
 })
