4aca86f4b39b36c12df8828eb29b501a9dff79b5
Ryan Lymburner <rlymbur@amazon.com>
2025-04-30 14:09:05 -0700

    Upgrade to actions/cache@v4

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 declare -a DEPENDENCY_LIST=("go" "awscli" "kubernetes-cli" "eksctl" "helm" "jq" "yq" "make")
 CURRENT_CONTROLLER_VERSION="1.1.0"
 CURRENT_CRD_VERSION="1.2.0"
-GOLANGCI_LINT_VERSION="1.63.4"
+GOLANGCI_LINT_VERSION="2.1.5"
 EKS_POD_IDENTITY_AGENT_VERSION="1.0.0-eksbuild.1"
 
 main() {
