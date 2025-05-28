d3143a2adb94f36e7840e9aa41625ecefe45c24c
Ryan Lymburner <rlymbur@amazon.com>
2025-05-28 12:21:12 -0700

    Increase suite timeout to 90 minutes

--- pkg/controllers/suite_test.go
+++ pkg/controllers/suite_test.go
@@ -67,7 +67,7 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
-}, 60)
+}, 90)
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
