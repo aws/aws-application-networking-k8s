bda2ad32adb568f9596e761700959b118bcbd989
vbedi <vbedi@amazon.com>
2025-09-29 22:18:43 +0000

    Release artifacts for release v1.1.5

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 declare -a DEPENDENCY_LIST=("go" "awscli" "kubernetes-cli" "eksctl" "helm" "jq" "yq" "make")
-CURRENT_CONTROLLER_VERSION="1.1.4"
+CURRENT_CONTROLLER_VERSION="1.1.5"
 CURRENT_CRD_VERSION="1.2.0"
 GOLANGCI_LINT_VERSION="2.4.0"
 EKS_POD_IDENTITY_AGENT_VERSION="1.0.0-eksbuild.1"
