eba02c63f728dd058fcdebdc24685ab0014ed50f
d-padmanabhan <88682350+d-padmanabhan@users.noreply.github.com>
2026-02-08 09:16:01 -0500

    fix(rbac): reduce pod permissions to least privilege
    - Limit pods to get/list/watch
    - Keep only pods/status update/patch where needed
    - Remove unused pods/finalizers rules

--- scripts/gen-webhook-secret.sh
+++ scripts/gen-webhook-secret.sh
@@ -6,8 +6,8 @@
 # Generates a certificate for use by the controller webhook
 # You can also manually re-create the webhook secret using your own PKI
 
-TEMP_KEY=`mktemp`
-TEMP_CERT=`mktemp`
+TEMP_KEY="$(mktemp)"
+TEMP_CERT="$(mktemp)"
 
 WEBHOOK_SVC_NAME=webhook-service
 WEBHOOK_NAME=aws-appnet-gwc-mutating-webhook
@@ -16,19 +16,19 @@ WEBHOOK_SECRET_NAME=webhook-cert
 
 echo "Generating certificate for webhook"
 HOST=${WEBHOOK_SVC_NAME}.${WEBHOOK_NAMESPACE}.svc
-openssl req -x509 -nodes -days 36500 -newkey rsa:2048 -keyout ${TEMP_KEY} -out ${TEMP_CERT} -subj "/CN=${HOST}/O=${HOST}" \
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "${TEMP_KEY}" -out "${TEMP_CERT}" -subj "/CN=${HOST}/O=${HOST}" \
    -addext "subjectAltName = DNS:${HOST}, DNS:${HOST}.cluster.local"
 
-CERT_B64=`cat $TEMP_CERT | base64`
+CERT_B64="$(base64 < "${TEMP_CERT}")"
 
 echo "Recreating webhook secret"
 kubectl delete secret $WEBHOOK_SECRET_NAME --namespace $WEBHOOK_NAMESPACE
-kubectl create secret tls $WEBHOOK_SECRET_NAME --namespace $WEBHOOK_NAMESPACE --cert=${TEMP_CERT} --key=${TEMP_KEY}
+kubectl create secret tls $WEBHOOK_SECRET_NAME --namespace $WEBHOOK_NAMESPACE --cert="${TEMP_CERT}" --key="${TEMP_KEY}"
 
 echo "Patching webhook with new cert"
 kubectl patch mutatingwebhookconfigurations.admissionregistration.k8s.io $WEBHOOK_NAME \
     --namespace $WEBHOOK_NAMESPACE --type='json' \
     -p="[{'op': 'replace', 'path': '/webhooks/0/clientConfig/caBundle', 'value': '${CERT_B64}'}]"
 
-rm $TEMP_KEY $TEMP_CERT
+rm -f "${TEMP_KEY}" "${TEMP_CERT}"
 echo "Done"
